a898db596e193e17e8519c9a2855e1a05b4f8166
jvegax <toledo.utrera@hotmail.com>
2023-04-07 19:28:23 +0200

    chore: setup expo av for audio

--- src/containers/Screens/Library/Content/index.tsx
+++ src/containers/Screens/Library/Content/index.tsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text } from '../../../../components'
 import { Container } from './styles'
+import { Audio } from 'expo-av';
+import { MOCK_CHART } from '../../../../models/Chart/mock';
+import { TouchableOpacity } from 'react-native';
+import theme from '../../../../theme';
 
 const Content = () => {
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
+
+  const playAudio = async () => {
+    if (!sound) return;
+    await sound.playAsync();
+  }
+
+  const stopAudio = async () => {
+    if (!sound) return;
+    await sound.stopAsync();
+  }
+
+  useEffect(() => {
+    async function loadAudio() {
+      const { sound } = await Audio.Sound.createAsync({
+        uri: MOCK_CHART.player.uri,
+      });
+      setSound(sound);
+    }
+    loadAudio();
+  }, []);
+
   return (
     <Container>
-      <Text size={22} weight={400}>Content</Text>
+      <TouchableOpacity onPress={playAudio}
+        style={{
+          backgroundColor: theme.colors.green,
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: 20
+        }}
+      >
+        <Text size={28} weight={600}>Play</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={stopAudio}
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                borderRadius: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: 20
+              }}
+      >
+        <Text size={26} weight={600}>Stop</Text>
+      </TouchableOpacity>
     </Container>
   )
 }
